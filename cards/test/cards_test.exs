994239cc7a2c43a93517be509f5e75c9f119c93e
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-01-19 00:31:07 -0600

    part 2

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -2,7 +2,9 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "create_deck creates 24 different cards" do
+    deck_length = length Cards.create_deck
+    assert deck_length == 24
   end
+
 end
