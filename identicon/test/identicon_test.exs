1e11f6b7bf31319c5846fba8d9b5da515a754f2b
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-01-21 04:01:10 -0600

    identicon main function testing.

--- identicon/test/identicon_test.exs
+++ identicon/test/identicon_test.exs
@@ -2,7 +2,15 @@ defmodule IdenticonTest do
   use ExUnit.Case
   doctest Identicon
 
-  test "greets the world" do
-    assert Identicon.hello() == :world
+  test "main returns all values not nil" do
+    image = Identicon.main("Eduardo")
+    assert image.hex != nil &&
+        image.color != nil &&
+        image.grid != nil
+  end
+
+  test "image contains a 3 color list in propery color" do
+    image = Identicon.main("Eduardo")
+    assert tuple_size(image.color) == 3
   end
 end
