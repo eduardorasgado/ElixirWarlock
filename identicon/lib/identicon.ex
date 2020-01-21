981ff5f9c26d2ce0a8da85bac09b9a9d7d38e18d
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-01-20 21:37:27 -0600

    Getting started with identicon project: converting names into images.

---
+++ identicon/lib/identicon.ex
@@ -0,0 +1,20 @@
+defmodule Identicon do
+  @doc """
+  Converts a string into an identicon
+  """
+
+  @doc """
+  Main function that will assemble the pipe process
+  """
+  def main(input) do
+    input 
+    |> hash_input
+  end
+
+  @doc """
+  Converts a string input into a hash md5 code string 
+  """
+  def hash_input(input) do
+    
+  end
+end
