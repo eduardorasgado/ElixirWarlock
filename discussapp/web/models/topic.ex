0c1c95d52b3aefef5d08742286f6464e738b9e1c
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-01-25 02:23:10 -0600

    adding comments to topic model

--- discussapp/web/models/topic.ex
+++ discussapp/web/models/topic.ex
@@ -9,9 +9,20 @@ defmodule Discussapp.Topic do
     field :title, :string
   end
 
-  def changeset(struct, params) do
+  @doc """
+  This function creates a Ecto.changeset struct with action, changes
+  and errors for validating new data for a db record change(in params)
+  and accordingly to actual record data(struct)
+
+  backward slash backward slash %{} is the way to assign default values in elixir
+
+  This function returns a changeset too
+  """
+  def changeset(struct, params \\ %{}) do
     struct
+    # records ow we want to update our database like the new props want to stick in there
     |> cast(params, [ :title ])
+    # make sure the changeset contents a title prop
     |> validate_required([ :title ])
   end
 
