450756dbac559f8cfc15b55b9b41788affbaaffa
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-01-25 00:33:27 -0600

    Creating a topic controller and creating topics/new function.

---
+++ discussapp/web/controllers/topic_controller.ex
@@ -0,0 +1,14 @@
+defmodule Discussapp.TopicController do
+  use Discussapp.Web, :controller
+  @moduledoc """
+  By convention controller controller should be named as single element:
+  TopicController and not TopicsController
+  """
+
+  @doc """
+  This function handles the form to create a new topic
+  """
+  def new(conn, _params) do
+    "hello to create new topic"
+  end
+end
--- discussapp/web/router.ex
+++ discussapp/web/router.ex
@@ -1,4 +1,16 @@
 defmodule Discussapp.Router do
+  @moduledoc """
+  Guide to all the conventions to route with phoenix:
+
+  user_path  GET     /users           HelloWeb.UserController :index
+  user_path  GET     /users/:id/edit  HelloWeb.UserController :edit
+  user_path  GET     /users/new       HelloWeb.UserController :new
+  user_path  GET     /users/:id       HelloWeb.UserController :show
+  user_path  POST    /users           HelloWeb.UserController :create
+  user_path  PATCH   /users/:id       HelloWeb.UserController :update
+            PUT     /users/:id       HelloWeb.UserController :update
+  user_path  DELETE  /users/:id       HelloWeb.UserController :delete
+  """
   use Discussapp.Web, :router
 
   pipeline :browser do
@@ -17,6 +29,8 @@ defmodule Discussapp.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    # method, action addess, controller, function in controller(convention)
+    get "/topics/new", TopicController, :new
   end
 
   # Other scopes may use custom stacks.
