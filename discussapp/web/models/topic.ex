d6d183c4e085b7163d0a7e85d8b991e25ac6af02
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-01-25 02:02:05 -0600

    Creating a changeset function in topic model

--- discussapp/web/models/topic.ex
+++ discussapp/web/models/topic.ex
@@ -3,9 +3,16 @@ defmodule Discussapp.Topic do
 
   @doc """
   Defining table data validation for our topic model
+  Topics is the table name as it was created in topics migration file
   """
   schema "topics" do
     field :title, :string
+  end
+
+  def changeset(struct, params) do
+    struct
+    |> cast(params, [ :title ])
+    |> validate_required([ :title ])
   end
 
 end
