6bf5684dfcb13467b139a6edd540256624a2a8f4
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-02-02 22:58:27 -0600

    the time in words challege completed for all corner cases.

--- hackerank_elixir/easy/the_time_in_words.ex
+++ hackerank_elixir/easy/the_time_in_words.ex
@@ -12,15 +12,25 @@ defmodule Solution do
   defp solution h_string, m_string do
     m = m_string
     |> String.to_integer
+    h = h_string |> String.to_integer
+
     minute = searching_element_in_time(m, get_minutes())
-    hour = h_string |> String.to_integer |> searching_element_in_time(get_hours())
+    hour = searching_element_in_time(h, get_hours())
     cond do
       m == 0 ->
         "#{hour} o' clock"
       m >= 1 && m <= 30 ->
-        "#{minute} past #{hour}"
+        # minute when 1 and minutes everything but quarter, quarter should have no company word
+        min_literal = if m != 1 and m != 15 and m != 30,
+                      do: " minutes ", else:
+                        if m == 1, do: " minute ", else: " "
+        "#{minute}#{min_literal}past #{hour}"
       m > 30 ->
-        "#{minute} minutes to #{hour}"
+        minute = searching_element_in_time(60-m, get_minutes())
+        hour = searching_element_in_time(rem(h+1, 12), get_hours())
+        # if quarter then string should not include minutes word
+        min_literal = if m != 45, do: " minutes ", else: " "
+        "#{minute}#{min_literal}to #{hour}"
     end
   end
 
@@ -54,6 +64,7 @@ defmodule Solution do
     |> Enum.map(fn(element) ->
       cond do
         element == "thirty" -> "half"
+        element == "fourty five" -> "quarter"
         true -> element
       end
     end)
@@ -74,14 +85,29 @@ defmodule Solution do
   end
 end
 
-# correct is: thirteen minutes to six
-Solution.main("5", "47")
+# correct is: one minute to six
+Solution.main("5", "01")
+
+# correct is: five minutes past seven
+Solution.main("7", "05")
 
 # correct is: three o' clock
 Solution.main("3", "00")
+
+# correct is: half past three
+Solution.main("3", "30")
 
 # correct is: quarter past seven
 Solution.main("7", "15")
 
 # correct is: half past five
 Solution.main("5", "30")
+
+# correct is: thirteen minutes to six
+Solution.main("5", "47")
+
+# correct is: thirteen minutes to one
+Solution.main("12", "47")
+
+# correct is: quarter to six
+Solution.main("5", "45")
