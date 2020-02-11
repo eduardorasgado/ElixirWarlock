43e291334c9a8e548e1470b549cdcaef32ddcffb
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-02-10 19:25:19 -0600

    Explaining what is the problem about.

---
+++ hackerank_elixir/easy/service_lane.ex
@@ -0,0 +1,24 @@
+defmodule Solution do
+  @moduledoc """
+  Implementing the solution for the next problem:
+    https://www.hackerrank.com/challenges/service-lane/problem
+  """
+
+  @doc """
+  function to receive official main to upload to hackerank
+  we will receive:
+
+  n, t
+  [width list]
+  i1, j1
+  i2, j2,
+  ...
+  where n is number of elements inside the w list
+  where t is the number of test
+  where i-th is the start index
+  where j-th is the end index of the segment been considered
+  """
+  def main do
+
+  end
+end
