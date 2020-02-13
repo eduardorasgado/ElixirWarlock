c2bf406be6806e0df26a3b1759805dd8ba1a7183
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-02-13 14:32:08 -0600

    Two to one problem: getting started.

---
+++ hackerank_elixir/easy/two_to_one.ex
@@ -0,0 +1,24 @@
+defmodule TwoToOne do
+  @moduledoc """
+  Can be found here:
+  https://www.codewars.com/kata/5656b6906de340bd1b0000ac/train/elixir
+
+  Take 2 strings s1 and s2 including only letters from ato z.
+  Return a new sorted string, the longest possible, containing distinct letters,
+
+    each taken only once - coming from s1 or s2.
+
+  """
+  def longest(a, b) do
+    # Put your code here
+  end
+end
+
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+#correct: "abcdefklmopqwxy"
+IO.puts longest(a, b)
+
+a = "abcdefghijklmnopqrstuvwxyz"
+#correct: "abcdefghijklmnopqrstuvwxyz"
+IO.puts longest(a, a)
