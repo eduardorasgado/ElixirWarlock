7a4e65de12a9f6b339c8337f22d00b60e32209d8
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-02-14 22:55:44 -0600

    valid braces: Fails last test.

--- hackerank_elixir/easy/valid_braces.ex
+++ hackerank_elixir/easy/valid_braces.ex
@@ -1,9 +1,10 @@
 defmodule Challenge do
   def valid_braces(braces) do
     b_reference = [{"(", ")"}, {"{", "}"}, {"[", "]"}]
-    m_p_m = braces |> String.graphemes
+    n_p_m = braces |> String.graphemes
     |> braces_analizer(b_reference, 0, 0)
-    rem(m_p_m, 2) == 0 && true || false
+    IO.puts "n_p_M: #{n_p_m  *2}, #{Enum.count(String.graphemes(braces))} "
+    n_p_m * 2 == Enum.count(String.graphemes(braces)) && true || false
   end
 
   def braces_analizer(braces, b_reference, index, n_pattern_matched) do
@@ -12,10 +13,24 @@ defmodule Challenge do
       actual_brace_pair = Enum.filter(b_reference, fn({i, _l}) ->
         i == Enum.fetch!(braces, index)
       end)
-      IO.inspect actual_brace_pair
-      # actual_brace_pair != nil &&
 
-      braces_analizer(braces, b_reference, index + 1, n_pattern_matched)
+      # adding indexes before recurtion
+      #braces_w_i = braces
+      #  |> Enum.with_index()
+
+      #IO.inspect actual_brace_pair
+
+      cond do
+        Enum.empty?(actual_brace_pair) == false ->
+          IO.puts "index + 1: #{index + 1}"
+          index_right_side = confirm_brace_closed(braces, actual_brace_pair, index + 1)
+          IO.inspect "diff: #{index_right_side - index - 1}"
+          incremets = rem(index_right_side - index - 1, 2) == 0 && n_pattern_matched + 1 || n_pattern_matched
+          braces_analizer(braces, b_reference, index + 1, incremets)
+        true ->
+          braces_analizer(braces, b_reference, index + 1, n_pattern_matched)
+      end
+
       true -> n_pattern_matched
     end
   end
@@ -26,6 +41,15 @@ defmodule Challenge do
   """
   def confirm_brace_closed(braces, actual_brace_pair, index) do
 
+    [{_i, l}] = actual_brace_pair
+
+    cond do
+      Enum.at(braces, index) == l || index >= Enum.count(braces)->
+        IO.inspect index
+        index
+       true -> confirm_brace_closed(braces, actual_brace_pair, index + 1)
+    end
+    # new index come here or nil, cuz there is a chance to get no no closing c brace
   end
 end
 IO.puts("----------")
@@ -38,3 +62,5 @@ IO.puts("----------")
 IO.inspect Challenge.valid_braces("[(])")
 IO.puts("----------")
 IO.inspect Challenge.valid_braces("[({})](]")
+IO.puts("----------")
+IO.inspect Challenge.valid_braces(")(}{][")
