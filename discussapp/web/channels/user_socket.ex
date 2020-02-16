37a447b2d2a441dd9df8f6da1ec52b708b3a4328
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-02-15 23:30:42 -0600

    creating comments channel to create a live load topic/channel feature.

--- discussapp/web/channels/user_socket.ex
+++ discussapp/web/channels/user_socket.ex
@@ -1,37 +1,30 @@
 defmodule Discussapp.UserSocket do
+  @moduledoc """
+  WebSockets is a protocol for exchanging dynamic and live information
+  between a client and a server
+
+  Phoenix supports websockets and long pooling
+
+  Long pooling is an alternative means of doinf that exact same thing but
+  works by using traditional http request
+  """
   use Phoenix.Socket
 
   ## Channels
   # channel "room:*", Discussapp.RoomChannel
+  # phoenix will handle everything that comes from  comments colon anything
+  # and be handle by CommentsChannel
+  channel "comments:*", Discussapp.CommentsChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
   # transport :longpoll, Phoenix.Transports.LongPoll
 
-  # Socket params are passed from the client and can
-  # be used to verify and authenticate a user. After
-  # verification, you can put default assigns into
-  # the socket that will be set for all channels, ie
-  #
-  #     {:ok, assign(socket, :user_id, verified_user_id)}
-  #
-  # To deny connection, return `:error`.
-  #
-  # See `Phoenix.Token` documentation for examples in
-  # performing token verification on connect.
+  # whenever a JS client connects to our server socket
   def connect(_params, socket) do
     {:ok, socket}
   end
 
-  # Socket id's are topics that allow you to identify all sockets for a given user:
-  #
-  #     def id(socket), do: "users_socket:#{socket.assigns.user_id}"
-  #
-  # Would allow you to broadcast a "disconnect" event and terminate
-  # all active sockets and channels for a given user:
-  #
-  #     Discussapp.Endpoint.broadcast("users_socket:#{user.id}", "disconnect", %{})
-  #
   # Returning `nil` makes this socket anonymous.
   def id(_socket), do: nil
 end
